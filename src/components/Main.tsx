ef9e21ff9392db012f22006e2303218f20e639a6
Partha-sarathi-07 <sarathicharu07@gmail.com>
2025-05-15 23:55:58 +0530

    ui component completed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,37 @@
 import './../styles/main.css';
+import clsx from 'clsx';
 import { languages } from '../languages'
+import { useState } from 'react';
+
 export default function Main() {
+    const [currentWord, setCurrentWord] = useState<string>("react");
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+    const alphabets = "abcdefghijklmnopqrstuvwxyz";
+    const currentWordElements = currentWord.split("").map((char, index) => 
+        <span key={index}>{char}</span>
+    );
+
+    const keyBoardElements = alphabets.split("").map(alphabet => {
+        return ( 
+            <button 
+                key={alphabet}
+                className={clsx({
+                        'correct-letter' : guessedLetters.includes(alphabet) && currentWord.includes(alphabet),
+                        'wrong-letter' : guessedLetters.includes(alphabet) && !currentWord.includes(alphabet)
+                    })}
+                onClick={() => hold(alphabet)}
+            >
+                {alphabet}
+            </button>
+        )
+    })
+    function hold(letter: string) {
+        setGuessedLetters(prevletters => 
+            prevletters.includes(letter) ? 
+                prevletters : 
+                [...prevletters, letter]
+        )
+    }
     const languagesElements = languages.map(language => 
                                 <div 
                                     key={language.name}
@@ -20,6 +51,13 @@ export default function Main() {
             <div id='languages'>
                 {languagesElements}
             </div>
+            <div id='current-word'>
+                {currentWordElements}   
+            </div>
+            <div id='keyboard'>
+                {keyBoardElements}
+            </div>
+            <button>New Game</button>
         </main>
     ) 
 }
